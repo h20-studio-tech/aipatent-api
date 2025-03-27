3bd78f7c04d2b4fcac8efce464e7ddf7334cde26
Victor Torrealba <vtorrealbalp@gmail.com>
2025-03-27 14:08:41 -0400

    🔧 Update AWS credential environment variable names
    - 🔐 Change AWS_ACCESS_KEY_ID to ACCESS_KEY_ID
    - 🔐 Change AWS_SECRET_ACCESS_KEY to SECRET_ACCESS_KEY

--- src/main.py
+++ src/main.py
@@ -361,8 +361,8 @@ async def patent(file: UploadFile):
 dynamodb = boto3.resource(
     "dynamodb",
     region_name=os.getenv("AWS_REGION", "us-east-1"),
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
 )
 
 
